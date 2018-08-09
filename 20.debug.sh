35ffe5e6dcfe2638d2cc11e1e557258597f11041
Andriy Berestovskyy <berestovskyy@gmail.com>
2018-08-09 18:45:15 +0200

    Fix debug build script It seems --define does not work with WHITESPACE_ONLY

--- 20.debug.sh
+++ 20.debug.sh
@@ -3,7 +3,7 @@
 . ./00.config.sh
 
 EXT_FILE_NAME="${EXT_FILE_NAME%.user.js}.debug.js"
-EXTRA_PARAMS="--define DEF_DEBUG=true --compilation_level WHITESPACE_ONLY"
+EXTRA_PARAMS="--define DEF_DEBUG=true --compilation_level SIMPLE_OPTIMIZATIONS"
 
 
 . ./99.build.sh
