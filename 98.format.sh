425c0315d8c26c9e7bda04c014de9ef488347874
Andriy Berestovskyy <berestovskyy@gmail.com>
2018-08-09 18:46:41 +0200

    Increase ColumnLimit format style To accomodate script meta description

--- 98.format.sh
+++ 98.format.sh
@@ -3,6 +3,6 @@
 echo "===> Formatting ${EXT_FILE_NAME}..."
 
 clang-format \
-	-style='{BasedOnStyle: Google, UseTab: Always, TabWidth: 4, ColumnLimit: 110}' \
+	-style='{BasedOnStyle: Google, UseTab: Always, TabWidth: 4, ColumnLimit: 200}' \
 	"${DST_DIR}/${EXT_FILE_NAME}" > "${DST_DIR}/${EXT_FILE_NAME}.fmt" \
 	&& mv -f "${DST_DIR}/${EXT_FILE_NAME}.fmt" "${DST_DIR}/${EXT_FILE_NAME}"
