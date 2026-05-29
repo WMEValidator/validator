c9c378098d04124fb490eb7f7c694a12ca1438cc
Miguel Vergara M. <127254828+mvergaram@users.noreply.github.com>
2026-05-29 19:04:28 -0400

    replace PDF_PEDIA for PDF_SEARCH

--- src/release.js
+++ src/release.js
@@ -125,7 +125,7 @@ var MAX_CHECKS = 310;
 /** @const */
 var PFX_WIKI = 'https://www.waze.com/wiki/';
 /** @const */
-var PFX_PEDIA = 'https://www.waze.com/discuss/search?q=';
+var PFX_SEARCH = 'https://www.waze.com/discuss/search?q=';
 /** @const */
 var PFX_FORUM = 'https://www.waze.com/forum/viewtopic.php?';
 /** @const */
