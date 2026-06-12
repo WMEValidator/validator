2a0c7417234c3180f72bac438c3d2c61d03664ed
Dave Alverson <daveacincy.waze@gmail.com>
2026-06-12 15:55:27 -0400

    Release 2026.06.12

--- 10.release.sh
+++ 10.release.sh
@@ -2,7 +2,8 @@
 
 . ./00.config.sh
 
-EXTRA_PARAMS="--compilation_level ADVANCED_OPTIMIZATIONS"
+#EXTRA_PARAMS="--compilation_level ADVANCED_OPTIMIZATIONS"
+EXTRA_PARAMS="--compilation_level WHITESPACE_ONLY"
 
 
 . ./99.build.sh
