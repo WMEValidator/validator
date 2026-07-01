7f50574db91df7ebd9078059f8ff5607658d3ab7
Miguel Vergara Martínez <8235478+miguelovergara@users.noreply.github.com>
2026-07-01 10:48:35 -0400

    Update version and whats new

--- src/release.js
+++ src/release.js
@@ -26,11 +26,14 @@
  * WV RELEASE INFORMATION
  */
 /** WME Validator version @const */
-var WV_VERSION = "2026.06.22";
+var WV_VERSION = "2026.07.01";
 /** Auto-save password. Increase to reset auto-save config. @const */
 var AS_PASSWORD = "v1";
 /** @const */
-var WV_WHATSNEW = `v2026.06.12:
+var WV_WHATSNEW = `v2026.07.01:
+- miguelovergara: updated issues links from waze forum old links to discuss search.
+
+v2026.06.12:
 - DaveAcincy: migrating validator to use the WME SDK.
 - update US and Chile localizations.
 
